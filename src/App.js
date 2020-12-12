edcbfb4e15fef5578ea181e00aa2f8f636baffdb
rwilt <rosie.wilt@gmail.com>
2020-12-12 18:36:33 -0500

    project skeleton set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+// import video from './paintbg.mp4';
+import video from './paintvidsmall.mp4';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <video autoPlay loop muted>
+          <source src={video} type="video/mp4"/>
+        </video>
+        <h1>Yukari Levine</h1>
+        <ul>
+          <li>Services</li>
+          <li>Clients</li>
+          <li>Contact</li>
+        </ul>
     </div>
   );
 }
