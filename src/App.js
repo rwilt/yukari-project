3ba1e9176b08cd62caa7671aa537b052afa7f122
rwilt <rosie.wilt@gmail.com>
2020-12-12 18:47:52 -0500

    added favicon

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import video from './paintvidsmall.mp4';
 function App() {
   return (
     <div className="App">
-
         <video autoPlay loop muted>
           <source src={video} type="video/mp4"/>
         </video>
+        <nav className="nav-container">
         <h1>Yukari Levine</h1>
         <ul>
           <li>Services</li>
           <li>Clients</li>
           <li>Contact</li>
         </ul>
+        </nav>
     </div>
   );
 }
